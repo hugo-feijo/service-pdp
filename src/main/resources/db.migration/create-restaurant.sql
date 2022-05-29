d03341dc628835a049d98b29becc0a8bb3383e9c
Hugo Feijó <hugo.ofeijo@gmail.com>
2022-05-29 16:09:57 -0300

    fix: persistence eclipselink

--- src/main/resources/db.migration/create-restaurant.sql
+++ src/main/resources/db.migration/create-restaurant.sql
@@ -1,11 +1,11 @@
-INSERT INTO pdp.restaurant (image_banner_url, name)
+INSERT INTO restaurant (image_banner_url, name)
 VALUES ('https://lovingnewyork.com.br/wp-content/uploads/2019/09/Manhatta-Restaurant-NYC-190909102021004.jpg',
         'Restaurante PDP');
 
-INSERT INTO pdp.restaurant_unity (name, id_restaurant)
+INSERT INTO restaurant_unity (name, id_restaurant)
 VALUES ('Centro', 1);
 
-INSERT INTO pdp.restaurant_table (name, id_restaurant_unity)
+INSERT INTO restaurant_table (name, id_restaurant_unity)
 VALUES ('Externo 01', 1),
        ('Externo 02', 1),
        ('Externo 03', 1),
