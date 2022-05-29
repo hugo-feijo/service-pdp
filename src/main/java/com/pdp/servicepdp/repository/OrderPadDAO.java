30aec565b59edc6ad506a49e96e445778b9eaf42
Hugo Feijó <hugo.ofeijo@gmail.com>
2022-05-29 17:25:27 -0300

    fix: change sql to get order pad by tableId and restaurantUnityId

--- src/main/java/com/pdp/servicepdp/repository/OrderPadDAO.java
+++ src/main/java/com/pdp/servicepdp/repository/OrderPadDAO.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 @Repository
 public class OrderPadDAO extends dao<OrderPad> {
-    public List<OrderPad> getOpenOrderPadByRestaurantUnityId(Integer restaurantUnityId) {
-        String JPQL="select op from OrderPad op WHERE op.table.id = ?1 AND op.closedAt IS NULL";
-        return super.read(JPQL, restaurantUnityId);
+    public List<OrderPad> getOpenedOrderPadByRestaurantUnityIdAndTableId(Integer restaurantUnityId, Integer tableId) {
+        String jpql="select op from OrderPad op WHERE op.table.id = ?1 AND op.table.restaurantUnity.id = ?2 AND op.closedAt IS NULL";
+        return super.read(jpql, tableId, restaurantUnityId);
     }
 }
