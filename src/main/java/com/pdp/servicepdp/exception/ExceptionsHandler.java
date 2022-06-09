cf8f9d8e59ac5c249d365d95642815ef17ab50f4
Hugo Feijó <hugo.ofeijo@gmail.com>
2022-06-09 20:45:44 -0300

    fix: adding status to global exception

--- src/main/java/com/pdp/servicepdp/exception/ExceptionsHandler.java
+++ src/main/java/com/pdp/servicepdp/exception/ExceptionsHandler.java
@@ -1,17 +1,14 @@
 package com.pdp.servicepdp.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.persistence.EntityNotFoundException;
-
 @ControllerAdvice
 public class ExceptionsHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<String> handleGlobalException(GlobalException ex) {
+        return ResponseEntity.status(ex.getHttpStatus()).body(ex.getMessage());
     }
 }
--- src/main/java/com/pdp/servicepdp/exception/GlobalException.java
+++ src/main/java/com/pdp/servicepdp/exception/GlobalException.java
@@ -4,9 +4,13 @@ import org.springframework.http.HttpStatus;
 
 public class GlobalException extends RuntimeException{
 
-    private HttpStatus httpStatus;
+    private final HttpStatus httpStatus;
     public GlobalException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 }
