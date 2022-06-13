f494c66179a1a21d6276c7fcbc9aef55aa89b648
Hugo Feijó <hugo.ofeijo@gmail.com>
2022-06-12 21:50:03 -0300

    feat: filter active clients in OrderPad

--- src/main/java/com/pdp/servicepdp/repository/OrderPadDAO.java
+++ src/main/java/com/pdp/servicepdp/repository/OrderPadDAO.java
@@ -2,17 +2,25 @@ package com.pdp.servicepdp.repository;
 
 import com.pdp.servicepdp.abstratas.Dados;
 import com.pdp.servicepdp.abstratas.dao;
+import com.pdp.servicepdp.model.Client;
 import com.pdp.servicepdp.model.OrderPad;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class OrderPadDAO extends dao<OrderPad> {
     public List<OrderPad> getOpenedOrderPadByTableId(Integer tableId) {
         String jpql="select op from OrderPad op WHERE op.restaurantTable.id = ?1 AND op.closedAt IS NULL";
         List<OrderPad> orders = super.read(jpql, tableId);
-        orders.forEach(x -> Dados.getEntityManager().refresh(x));
+        orders.forEach(order -> {
+            Dados.getEntityManager().refresh(order);
+            if(order.getClients() != null){
+                order.getClients().forEach(client -> Dados.getEntityManager().refresh(client));
+                order.setClients(order.getClients().stream().filter(Client::getActive).collect(Collectors.toSet()));
+            }
+        });
         return orders;
     }
 }
